cca15ea3793761ae07e0b02d4990408af4b47d47
shindeswaraj10 <shindeswaraj10@gmail.com>
2022-03-11 11:35:04 +0530

    SampleTest Created

---
+++ SampleTest.py
@@ -0,0 +1,8 @@
+from locust import User, task, between, wait_time
+
+class MyUser(User):
+    wait_time = between(5,15)
+
+    @task
+    def my_task(self):
+        print("Its my first sample task")
